1652018676a12cfabd57e666dfe91b249b775acd
Andrew G <122369063+MineKing702@users.noreply.github.com>
2026-04-25 16:32:50 -0400

    Update PlayerSettings.cs

--- Runtime/Settings/PlayerSettings.cs
+++ Runtime/Settings/PlayerSettings.cs
@@ -1,1 +1,10 @@
-
+namespace CursedDepths.Core.Settings
+{
+    public class PlayerSettings
+    {
+        // Volume Settings
+        public float MasterVolume = 100f;
+        public float MusicVolue = 100f;
+        public float SoundEffects = 100f;
+    }
+}
