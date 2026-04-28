d3966e1d2d7f1b1b2b50ad78cab739bfbe48cb1e
Andrew G <122369063+MineKing702@users.noreply.github.com>
2026-04-28 11:52:46 -0400

    Add files via upload

---
+++ SettingsSavedEventArgs.cs
@@ -0,0 +1,11 @@
+using CursedDepths.Core.Settings;
+
+public class SettingsSavedEventArgs
+{
+    public PlayerSettings playerSettings
+
+    public SettingSavedEventArgs(PlayerSettings playerSettings)
+    {
+        this.playerSettings = playerSettings;
+    }
+}
