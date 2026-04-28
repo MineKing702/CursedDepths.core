45d7436628d30b4e4520abdf0b6e7b5f17e827b4
Andrew G <122369063+MineKing702@users.noreply.github.com>
2026-04-28 11:55:35 -0400

    Update SettingsSavedEventArgs.cs

--- Runtime/EventArgs/SettingsSavedEventArgs.cs
+++ Runtime/EventArgs/SettingsSavedEventArgs.cs
@@ -2,7 +2,7 @@ using CursedDepths.Core.Settings;
 
 public class SettingsSavedEventArgs
 {
-    public PlayerSettings playerSettings
+    public PlayerSettings playerSettings;
 
     public SettingSavedEventArgs(PlayerSettings playerSettings)
     {
