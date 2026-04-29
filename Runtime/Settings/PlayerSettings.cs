1536f9a95f4ff183565c95cccd8acbc323014d09
Andrew G <122369063+MineKing702@users.noreply.github.com>
2026-04-29 11:42:50 -0400

    Update PlayerSettings.cs

--- Runtime/Settings/PlayerSettings.cs
+++ Runtime/Settings/PlayerSettings.cs
@@ -6,5 +6,11 @@ namespace CursedDepths.Core.Settings
         public float MasterVolume { get; set; } 
         public float MusicVolume { get; set; } 
         public float SoundEffects { get; set; } 
+
+        // Keybind Settings
+        public Keycode WalkLeft { get; set; }
+        public Keycode WalkRight { get; set; }
+        public Keycode Jump { get; set; }
+        public Keycode Attack { get; set; }
     }
 }
